806a53f8e1737dedddf679786bae25646c75e22c
guptaRishi00 <rishigupta54066@gmail.com>
2026-03-05 13:49:31 +0530

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,14 +22,15 @@ const Navbar = ({ onOpenPopup }: NavbarProps) => {
     "px-6 py-2.5 rounded-full text-[14px] font-medium border cursor-pointer";
   const buttonIdle = "text-gray-700 border-gray-300 bg-transparent";
   const buttonHover =
-    " hover:text-white hover:border-transparent hover:bg-gradient-to-r hover:from-[#3445E7] hover:via-[#2F85EA] hover:to-[#07D6F3]";
+    " hover:text-white hover:border-white hover:bg-gradient-to-r hover:from-[#3445E7] hover:via-[#2F85EA] hover:to-[#07D6F3]";
 
   return (
     <nav
-      className={`fixed top-0 w-full z-50 ${scrolled
-        ? "py-3 bg-white/95 backdrop-blur-xl shadow-[0_1px_3px_rgba(0,0,0,0.05)]"
-        : "py-6 bg-transparent"
-        }`}
+      className={`fixed top-0 w-full z-50 ${
+        scrolled
+          ? "py-3 bg-white/95 backdrop-blur-xl shadow-[0_1px_3px_rgba(0,0,0,0.05)]"
+          : "py-6 bg-transparent"
+      }`}
     >
       <div className="max-w-343 mx-auto px-6 lg:px-16 flex justify-between items-center">
         {/* LEFT: Logo */}
