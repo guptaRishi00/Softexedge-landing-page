28856080269edd2bd1f0bee276605dd901cd9e1d
guptaRishi00 <rishigupta54066@gmail.com>
2026-04-03 12:21:50 +0530

    first commit

--- src/components/SchedulePicker.tsx
+++ src/components/SchedulePicker.tsx
@@ -34,9 +34,9 @@ export default function SchedulePicker({ onSelect, selectedDate, selectedTime }:
   const visibleDates = allDates.slice(page * datesPerPage, (page + 1) * datesPerPage);
 
   const timeSlots = {
-    Morning: ["09:00 AM", "09:30 AM", "10:00 AM", "10:30 AM", "11:00 AM"],
-    Midday: ["12:00 PM", "12:30 PM", "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM", "03:00 PM"],
-    Evening: ["04:00 PM", "04:30 PM", "05:00 PM", "05:30 PM"],
+    Morning: ["10:30 AM", "11:00 AM", "11:30 AM"],
+    Midday: ["12:00 PM", "12:30 PM", "01:00 PM", "03:00 PM", "03:30 PM"],
+    Evening: ["04:00 PM", "04:30 PM", "05:00 PM", "05:30 PM", "06:00 PM", "06:30 PM", "07:00 PM", "07:30 PM", "08:00 PM"],
   };
 
   useEffect(() => {
